ce80f8e4b9f2c74f5c0f5df75331ef4bf8fc03b4
taknaja <peerapolkoeram@gmail.com>
2025-07-20 16:47:18 +0700

    add Project

---
+++ src/test/java/takee/dev/soapExample/SoapExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package takee.dev.soapExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
